5ed6fb85dee4a02ddb89c21e95a75b569e41680d
Shaw <cuzval@gmail.com>
2021-01-29 15:55:05 +0800

    Secondary link

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,23 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <div class="row">
-          <div class="col-xs-offset-2 col-xs-8">
-            <div class="page-header">
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
               <Header />
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-xs-2 col-xs-offset-2">
-            <div class="list-group">
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
               <AppNavLink to="/about">About</AppNavLink>
               <AppNavLink to="/home">Home</AppNavLink>
             </div>
           </div>
-          <div class="col-xs-6">
-            <div class="panel">
-              <div class="panel-body">
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
                 <Switch>
                   <Route path="/about" component={About} />
                   <Route path="/home" component={Home} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
+import {Switch, Redirect, Route} from 'react-router-dom'
+import Message from './Home/Message'
+import News from './Home/News'
+import AppNavLink from '../components/AppNavLink'
 
 export default class Home extends Component {
   render() {
     return (
-      <h3>This is Home</h3>
+      <div>
+        <h3>This is Home</h3>
+        <div>
+          <ul className="nav nav-tabs">
+            <li>
+              <AppNavLink to="/home/news">News</AppNavLink>
+            </li>
+            <li>
+              <AppNavLink to="/home/message">Message</AppNavLink>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/home/news" component={News}/>
+            <Route path="/home/message" component={Message}/>
+            <Redirect to="/home/news" />
+          </Switch>
+        </div>
+      </div>
     )
   }
 }
